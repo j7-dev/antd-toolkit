d2b878ac86546427fa911f549a205eee63c12df6
TW-IT-Jerry Liu <jerry.liu@shoalter.com>
2023-10-12 16:59:27 +0800

    feat: add hooks useColor

--- lib/hooks/useColor/index.stories.tsx
+++ lib/hooks/useColor/index.stories.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import type { Meta, StoryObj } from "@storybook/react";
-import { Title, Subtitle, Description, Stories } from "@storybook/blocks";
+import { Title, Subtitle, Description } from "@storybook/blocks";
 
 import { Components } from "./Components";
 
