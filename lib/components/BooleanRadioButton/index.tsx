faea2948f6483e6b12a9a1c11f9d4340c8de8a9d
J7 <j7.dev.gg@outlook.com>
2024-04-04 15:24:59 +0800

    feat: add initial value

--- lib/components/BooleanRadioButton/index.tsx
+++ lib/components/BooleanRadioButton/index.tsx
@@ -4,12 +4,12 @@ import { Form, Radio, FormItemProps, RadioGroupProps  } from "antd";
 import "./styles.scss";
 
 
-const defaultOptions: Array<{ label: ReactNode; value: string; disabled?: boolean; }> = [
-	{ label: 'ALL', value: '' },
+type TOption = { label: ReactNode; value: string; disabled?: boolean; }
+const defaultOptions: TOption[] = [
+	{ label: 'ALL', value: ''},
 	{ label: <CheckOutlined />, value: '1' },
 	{ label: <CloseOutlined />, value: '0' }
 ]
-
 
 export const BooleanRadioButton: React.FC<{
   formItemProps: FormItemProps;
@@ -17,11 +17,19 @@ export const BooleanRadioButton: React.FC<{
   averageWidth?: boolean;
 }> = ({ formItemProps, radioGroupProps, averageWidth = true }) => {
 const options = radioGroupProps?.options || defaultOptions;
+const optionType = options.some((option) => typeof option === "object") ? "object" : "primitive";
 
+const getInitialValue = () => {
+	if (optionType === "object") {
+		return (options as TOption[]).find((option) => true !== option?.disabled)?.value;
+	}
+	return options[0];
+}
 
   return (
-    <Form.Item {...formItemProps}>
+    <Form.Item initialValue={getInitialValue()} {...formItemProps}>
       <Radio.Group
+				defaultValue={getInitialValue()}
         optionType="button"
         buttonStyle="solid"
         className={averageWidth ? `w-avg` : ""}
