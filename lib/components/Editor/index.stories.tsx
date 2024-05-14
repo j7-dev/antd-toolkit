dcba01564d7e816f78beffcdf1475a2f6767cf84
J7 <j7.dev.gg@gmail.com>
2024-05-14 17:46:22 +0800

    feat: add YooptaEditor

---
+++ lib/components/Editor/index.stories.tsx
@@ -0,0 +1,36 @@
+import type { Meta, StoryObj } from "@storybook/react";
+
+import { Editor } from "./index";
+
+// More on how to set up stories at: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
+const meta: Meta<typeof Editor> & {
+  argTypes: any;
+} = {
+  title: "編輯器/Editor",
+  component: Editor,
+  parameters: {
+    // Optional parameter to center the component in the Canvas. More info: https://storybook.js.org/docs/react/configure/story-layout
+    layout: "centered",
+    docs: {
+      description: {
+        component: `
+
+				`, // 可以寫 markdown
+      },
+    },
+  },
+  tags: ["autodocs"],
+  argTypes: {
+  },
+};
+
+export default meta;
+type Story = StoryObj<typeof Editor>;
+
+// More on writing stories with args: https://storybook.js.org/docs/react/writing-stories/args
+export const General: Story = {
+  name: "一般用法",
+  args: {
+  },
+};
+
