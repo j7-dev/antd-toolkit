4a2d2e28e4e7be89ae1fe9e4eceb0567b3b55760
J7 <j7.dev.gg@gmail.com>
2024-08-12 16:59:34 +0800

    feat: 修改 LoadingCard Props

--- lib/components/LoadingCard/index.tsx
+++ lib/components/LoadingCard/index.tsx
@@ -2,18 +2,17 @@ import React, { HTMLAttributes } from 'react'
 
 type LoadingCardProps = {
   ratio?: string
-  text?: string
 } & HTMLAttributes<HTMLDivElement>
 
 export const LoadingCard: React.FC<LoadingCardProps> = ({
   ratio = 'aspect-video',
-  text = 'LOADING...',
+  children = 'LOADING...',
   ...rest
 }) => (
   <div
     className={`${ratio} bg-gray-200 animate-pulse rounded-lg py-[21px] pl-[28px] pr-6 relative mb-[10px] text-gray-500 flex items-center justify-center`}
     {...rest}
   >
-    {text}
+    {children}
   </div>
 )
