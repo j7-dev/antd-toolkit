a378db1fcdde233f7367643e53d2e7f5ddd39955
J7 <j7.dev.gg@gmail.com>
2025-01-16 17:54:39 +0800

    feat: move refine files

--- lib/refine/index.tsx
+++ lib/refine/index.tsx
@@ -2,3 +2,5 @@ import '@/assets/scss/index.scss'
 
 export * from '@/refine/components'
 export * from '@/refine/hooks'
+export * from '@/refine/dataProvider'
+export * from '@/refine/utils'
--- lib/utils/functions/index.tsx
+++ lib/utils/functions/index.tsx
@@ -1,7 +1,6 @@
 export * from './antd'
 export * from './api'
 export * from './common'
-export * from './refine'
 export * from './woocommerce'
 export * from './wordpress'
 export * from './dayjs'
