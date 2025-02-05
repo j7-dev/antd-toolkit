573b2863b9228c92e5542c4e79d603dbcba64437
J7 <j7.dev.gg@gmail.com>
2025-02-05 16:44:21 +0800

    refactor: PluginProvider

--- lib/main/components/PluginProvider/index.tsx
+++ lib/main/components/PluginProvider/index.tsx
@@ -1,18 +1,32 @@
-import React, { FC, useLayoutEffect } from 'react'
+import React, { useEffect } from 'react'
 import { useSetAtom } from 'jotai'
 import { pluginAtom, TEnv } from './atom'
+import { Provider } from 'jotai'
 
-const PluginProviderComponent: FC<{
+type Props = {
 	children: React.ReactNode
 	env: TEnv
-}> = ({ children, env }) => {
-	const setPluginAtom = useSetAtom(pluginAtom)
-	useLayoutEffect(() => {
-		setPluginAtom(env)
-	}, [env])
-	return <>{children}</>
 }
 
-export const PluginProvider = PluginProviderComponent
+// 建立一個內部組件來處理 env 的更新
+const PluginUpdater = ({ env }: { env: TEnv }) => {
+	const setPlugin = useSetAtom(pluginAtom)
+
+	useEffect(() => {
+		setPlugin(env)
+	}, [env, setPlugin])
+
+	return null
+}
+
+export const PluginProvider = ({ children, env }: Props) => {
+	return (
+		<Provider>
+			<PluginUpdater env={env} />
+			{children}
+		</Provider>
+	)
+}
+
 export * from './atom'
 export * from './hooks'
