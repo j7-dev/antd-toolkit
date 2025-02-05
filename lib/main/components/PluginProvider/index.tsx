5a9ac9afd65378bed77b505e3b27eb46f3d94841
J7 <j7.dev.gg@gmail.com>
2025-02-05 15:33:54 +0800

    feat: 不需要做解密

--- lib/main/components/PluginProvider/index.tsx
+++ lib/main/components/PluginProvider/index.tsx
@@ -2,24 +2,15 @@ import React, { FC, useLayoutEffect } from 'react'
 import { Provider } from 'jotai'
 import { pluginStore, pluginAtom, TEnv } from './atom'
 import { usePlugin } from './hooks'
-import { simpleDecrypt } from '@/main/utils'
 
 const PluginProviderComponent: FC<{
 	children: React.ReactNode
-	env: TEnv // 包含加密後的環境變數
+	env: TEnv
 }> & {
 	usePlugin: typeof usePlugin
 } = ({ children, env }) => {
 	useLayoutEffect(() => {
-		const decryptedEnv = {
-			...env,
-			// 以下幾個參數都需要解密
-			NONCE: simpleDecrypt(env.NONCE),
-			BUNNY_LIBRARY_ID: simpleDecrypt(env.BUNNY_LIBRARY_ID),
-			BUNNY_CDN_HOSTNAME: simpleDecrypt(env.BUNNY_CDN_HOSTNAME),
-			BUNNY_STREAM_API_KEY: simpleDecrypt(env.BUNNY_STREAM_API_KEY),
-		}
-		pluginStore.set(pluginAtom, decryptedEnv)
+		pluginStore.set(pluginAtom, env)
 	}, [env])
 	return <Provider store={pluginStore}>{children}</Provider>
 }
