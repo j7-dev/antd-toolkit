740f0e5c3cb533b2a0102e1db6eead49d2e72852
J7 <j7.dev.gg@gmail.com>
2025-02-07 18:03:09 +0800

    fix: 沒有 export 到 refine/utils/woocommerce

--- lib/refine/utils/index.tsx
+++ lib/refine/utils/index.tsx
@@ -1,6 +1,8 @@
 import { BaseRecord, CrudFilters } from '@refinedev/core'
 import { DeleteButtonProps } from '@refinedev/antd'
 
+export * from './woocommerce'
+
 /**
  * 將 key-value 的物件轉換成 refine 的 crudFilters
  * 以便執行 refine 的 crud 操作
