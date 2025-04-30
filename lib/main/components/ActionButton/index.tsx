9271faa5c3c156b285ba72e359722353cf4230f8
J7 <j7.dev.gg@gmail.com>
2025-04-30 11:39:10 +0800

    refactor: ActionButton

--- lib/main/components/ActionButton/index.tsx
+++ lib/main/components/ActionButton/index.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useState, memo } from 'react'
 import { Button, ButtonProps, Popconfirm } from 'antd'
 import {
 	DeleteOutlined,
@@ -7,7 +7,7 @@ import {
 	EditOutlined,
 } from '@ant-design/icons'
 
-type TActionButton = {
+export type TActionButton = {
 	canDelete?: boolean
 	buttonProps?: ButtonProps
 	className?: string
@@ -23,8 +23,7 @@ const defaultButtonProps = {
 	className: 'at-mx-1',
 }
 
-// TODO 需要留嗎?
-export const ActionButton = ({
+const ActionButtonComponent = ({
 	canDelete = true,
 	buttonProps = {},
 	className,
@@ -51,23 +50,23 @@ export const ActionButton = ({
 
 	const buttonPropsMapping = {
 		edit: {
-			children: 'Edit',
+			children: '編輯',
 			onClick: handleEdit,
 			icon: <EditOutlined />,
 		},
 		save: {
-			children: 'Save',
+			children: '儲存',
 			icon: <SaveOutlined />,
 			onClick: onSave,
 		},
 		cancel: {
-			children: 'Cancel',
+			children: '取消',
 			onClick: handleCancel,
 			icon: <CloseOutlined />,
 			type: 'default',
 		},
 		delete: {
-			children: 'Delete',
+			children: '刪除',
 			danger: true,
 			icon: <DeleteOutlined />,
 		},
@@ -120,3 +119,41 @@ export const ActionButton = ({
 		</div>
 	)
 }
+
+/**
+ * ActionButton 組件
+ *
+ * @description 提供編輯、保存、取消和刪除功能的按鈕組合組件
+ *
+ * @param {boolean} [isEditing=false] - 是否處於編輯模式
+ * @param {Function} [onEdit] - 點擊編輯按鈕時的回調函數
+ * @param {Function} [onSave] - 點擊保存按鈕時的回調函數
+ * @param {Function} [onCancel] - 點擊取消按鈕時的回調函數
+ * @param {Function} [onDelete] - 點擊刪除按鈕時的回調函數
+ * @param {boolean} [canDelete=true] - 是否顯示刪除按鈕
+ * @param {string} [className] - 容器的自定義 CSS 類名
+ * @param {ButtonProps} [buttonProps] - 所有按鈕的共用屬性
+ * @param {object} [buttonPropsMapping] - 各按鈕的個別屬性配置
+ * @param {'both'|'icon'|'text'} [type='both'] - 按鈕顯示類型：同時顯示圖標和文字、僅圖標或僅文字
+ *
+ * @returns {JSX.Element} 返回按鈕組合組件
+ *
+ * @example
+ * // 基本用法
+ * <ActionButton
+ *   onEdit={() => setIsEditing(true)}
+ *   onSave={handleSave}
+ *   onCancel={() => setIsEditing(false)}
+ * />
+ *
+ * // 僅顯示圖標
+ * <ActionButton
+ *   type="icon"
+ *   isEditing={isEditing}
+ *   onEdit={() => setIsEditing(true)}
+ *   onSave={handleSave}
+ *   onCancel={() => setIsEditing(false)}
+ * />
+ */
+
+export const ActionButton = memo(ActionButtonComponent)
