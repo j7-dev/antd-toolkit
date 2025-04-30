9ab94bfadf81ef3df06c64177b52f7810a777746
J7 <j7.dev.gg@gmail.com>
2025-04-30 12:09:41 +0800

    chore

--- lib/wp/components/general/MediaLibrary/index.tsx
+++ lib/wp/components/general/MediaLibrary/index.tsx
@@ -3,13 +3,6 @@ import List from '@/wp/components/general/MediaLibrary/List'
 import { TMediaLibraryProps } from '@/wp/components/general/MediaLibrary/types'
 import { MediaLibraryContext } from '@/wp/components/general/MediaLibrary/hooks/useProps'
 
-/**
- * WordPress 媒體庫元件
- * 提供影片上傳、管理和設定功能
- * @component
- * @param {TMediaLibraryProps} props - 元件屬性
- * @returns {JSX.Element} MediaLibrary 元件
- */
 const MediaLibraryComponent: FC<TMediaLibraryProps> = (props) => {
 	return (
 		<MediaLibraryContext.Provider value={props}>
@@ -18,4 +11,11 @@ const MediaLibraryComponent: FC<TMediaLibraryProps> = (props) => {
 	)
 }
 
+/**
+ * WordPress 媒體庫元件
+ * 提供影片上傳、管理和設定功能
+ * @component
+ * @param {TMediaLibraryProps} props - 元件屬性
+ * @returns {JSX.Element} MediaLibrary 元件
+ */
 export const MediaLibrary = memo(MediaLibraryComponent)
