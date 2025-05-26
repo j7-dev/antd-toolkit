e4b779449a18701b80eec238f767fab14812d79f
J7 <j7.dev.gg@gmail.com>
2025-05-26 17:15:38 +0800

    feat: 移除 BlockNote lazy

--- lib/main/components/editor/BlockNote/index.tsx
+++ lib/main/components/editor/BlockNote/index.tsx
@@ -1,20 +1,12 @@
-import { FC, lazy, Suspense } from 'react'
+import { FC } from 'react'
 import '@blocknote/core/fonts/inter.css'
 import { BlockNoteViewProps } from '@blocknote/react'
+import { BlockNoteView } from '@blocknote/mantine'
 import { DefaultStyleSchema, DefaultInlineContentSchema } from '@blocknote/core'
 import '@blocknote/mantine/style.css'
 import { schema } from './useBlockNote'
 import './index.scss'
-import { Button } from 'antd'
-import { LoadingOutlined } from '@ant-design/icons'
-
 export * from './useBlockNote'
-
-const BlockNoteView = lazy(() =>
-	import('@blocknote/mantine').then((module) => ({
-		default: module.BlockNoteView,
-	})),
-)
 
 /**
  * TODO
@@ -27,16 +19,5 @@ export const BlockNote: FC<
 		DefaultStyleSchema
 	>
 > = (blockNoteViewProps) => {
-	return (
-		<Suspense
-			fallback={
-				<Button type="text" icon={<LoadingOutlined />}>
-					Loading...
-				</Button>
-			}
-		>
-			{/* @ts-ignore */}
-			<BlockNoteView {...blockNoteViewProps} />
-		</Suspense>
-	)
+	return <BlockNoteView {...blockNoteViewProps} />
 }
