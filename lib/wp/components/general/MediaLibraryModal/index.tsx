e5e8700cc4bca7286ac5585e5b39e1a60d9225f4
J7 <j7.dev.gg@gmail.com>
2025-05-26 19:32:09 +0800

    feat: 優化 Modal ，延遲媒體庫載入順序，讓 Modal 不要卡住

--- lib/wp/components/general/MediaLibraryModal/index.tsx
+++ lib/wp/components/general/MediaLibraryModal/index.tsx
@@ -1,4 +1,4 @@
-import { memo } from 'react'
+import { memo, useEffect, useState } from 'react'
 import { Modal, ModalProps } from 'antd'
 import { MediaLibrary, TMediaLibraryProps } from '@/wp'
 
@@ -11,10 +11,22 @@ const MediaLibraryModalComponent = ({
 	modalProps: ModalProps
 	mediaLibraryProps: TMediaLibraryProps
 }) => {
+	const [show, setShow] = useState(false)
+
+	useEffect(() => {
+		const delay = setTimeout(() => {
+			if (modalProps.open) {
+				setShow(true)
+			}
+		}, 100)
+
+		return () => clearTimeout(delay)
+	}, [modalProps.open])
+
 	return (
 		<Modal {...modalProps}>
 			<div className="max-h-[75vh] overflow-x-hidden overflow-y-auto pr-4">
-				<MediaLibrary {...mediaLibraryProps} />
+				{show && <MediaLibrary {...mediaLibraryProps} />}
 			</div>
 		</Modal>
 	)
