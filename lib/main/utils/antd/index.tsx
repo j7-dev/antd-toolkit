a43eee891a03586f77d1395963fc75181d49a70d
J7 <j7.dev.gg@gmail.com>
2025-06-27 17:40:52 +0800

    feat: 新增 formatDateRangeData

--- lib/main/utils/antd/index.tsx
+++ lib/main/utils/antd/index.tsx
@@ -1,5 +1,6 @@
 import { TGetColumnFilterProps } from '@/main/types'
 import { PaginationProps } from 'antd'
+import dayjs, { Dayjs } from 'dayjs'
 
 export * from './constants'
 
@@ -71,3 +72,47 @@ export const getDefaultPaginationProps = ({
 	showTotal: (total: number, range: [number, number]) =>
 		`目前顯示第 ${range?.[0]} ~ ${range?.[1]} 個 ${label}，總共有 ${total} 個 ${label}`,
 })
+
+/**
+ * 處理日期欄位 sale_date_range
+ * 將表單中的 date Range
+ * 轉換成個別的 開始、結束 property
+ * 例如
+ * 表單中的 sale_date_range 是 [null, null]
+ * 轉換成 { date_on_sale_from: null, date_on_sale_to: null }
+ * @param values
+ * @param fromProperty
+ * @param toProperty
+
+ * @return
+ */
+export const formatDateRangeData = (
+	values: {
+		[key: string]: any
+	},
+	fromProperty: string,
+	toProperty: [string, string],
+) => {
+	const sale_date_range = values?.[fromProperty] || [null, null]
+
+	// 處理日期欄位 sale_date_range
+
+	const date_on_sale_from = dayjs.isDayjs(sale_date_range[0])
+		? (sale_date_range[0] as Dayjs).unix()
+		: sale_date_range[0]
+	const date_on_sale_to = dayjs.isDayjs(sale_date_range[1])
+		? (sale_date_range[1] as Dayjs).unix()
+		: sale_date_range[1]
+
+	const toPropertyFrom = toProperty[0]
+	const toPropertyTo = toProperty[1]
+
+	const formattedValues = {
+		...values,
+		[toPropertyFrom]: date_on_sale_from,
+		[toPropertyTo]: date_on_sale_to,
+		sale_date_range: undefined,
+	}
+
+	return formattedValues
+}
